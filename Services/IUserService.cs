a8f1f9cdab1be27ccee2baa625732c81f25dde21
David Perjans <perjansdavid@gmail.com>
2025-03-24 11:09:45 +0100

    I installed BCrypt for hashing the passwords when a user register.
    I use Automapper to map the DTOs
    
    I have also installed JWT and uses it to have a token for the user to authorize to be able to use certain endpoints that are protected.

---
+++ Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using BookSwap.DTOs;
+
+namespace BookSwap.Services
+{
+    public interface IUserService
+    {
+        Task<(bool IsSuccess, string ErrorMessage)> RegisterAsync (RegisterDto registerDto);
+        Task<(bool IsSuccess, string Token, string ErrorMessage)> LoginAsync(LoginDto loginDto);
+    }
+}
